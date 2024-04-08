582f6b3a43ff906c04196c04ca89b4bec62d6f8f
Charlotte Stone <charlie.stone649@gmail.com>
2024-04-08 11:43:12 +0100

    Create insert function

--- classes/note.php
+++ classes/note.php
@@ -11,6 +11,31 @@
             $this->conn = $db;
         }
         
+        public function runQuery($sql) {
+            $stmt = $this->conn->prepare($sql);
+            return $stmt;
+        }
+
+        /**
+         * The function is for inserting a new note by taking a title, content and due_date as parameters.
+         * @param string $title
+         * @param string $content
+         * @param string $due_date
+         * @return boolean
+         */
+        public function insert ($title, $content, $due_date) {
+            try {
+                $stmt = $this->conn->prepare("INSERT INTO notes (title, content, due_date) VALUES (:title, :content, :due_date)");
+                $stmt->bindparam(":title", $title);
+                $stmt->bindparam(":content", $content);
+                $stmt->bindparam(":due_date", $due_date);
+                $stmt->execute();
+                return true;
+            } catch (PDOException $e) {
+                echo $e->getMessage();
+                return false;
+            }
+        }
 
     }
 
