1525d56d8c92c3d74d0357787da82298d83d26b7
Charlotte Stone <charlie.stone649@gmail.com>
2024-04-08 18:05:52 +0100

    Create base of index page

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Form for making a note with OrganizeMe" />
+    <title>OrganizeMe</title>
+    <link qrel="stylesheet" type="text/css" href="../css/styles.css">
+</head>
+<body>
+<?php include "./includes/navbar.php" ?>
+
+<?php include "./includes/footer.php" ?>
+</body>
