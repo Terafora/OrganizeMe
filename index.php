d57eacedfa57ad6b8b5e61ac69e0752dcc2327c4
Charlotte Stone <charlie.stone649@gmail.com>
2024-04-08 18:39:33 +0100

    Add contents of else statement

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="Form for making a note with OrganizeMe" />
     <title>OrganizeMe</title>
-    <link qrel="stylesheet" type="text/css" href="../css/styles.css">
+    <link qrel="stylesheet" type="text/css" href="./css/styles.css">
 </head>
 <body>
     <?php include "./includes/navbar.php" ?>
@@ -46,7 +46,7 @@
                     </div>
                 <?php } ?>
             <?php } else { ?>
-                <p>No notes found</p>
+                <h3>Create a new note below</h3>
             <?php } ?>
         </div>
     </main>
