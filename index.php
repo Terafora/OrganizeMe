8cf7f025a97ec5850c81e2fa77f61672f933fbc5
Charlotte Stone <charlie.stone649@gmail.com>
2024-04-08 18:33:58 +0100

    Insert form notes onto index page.

--- index.php
+++ index.php
@@ -8,7 +8,48 @@
     <link qrel="stylesheet" type="text/css" href="../css/styles.css">
 </head>
 <body>
-<?php include "./includes/navbar.php" ?>
+    <?php include "./includes/navbar.php" ?>
 
-<?php include "./includes/footer.php" ?>
+    <main role="main" >
+        <?php
+            $query = "SELECT * FROM notes";
+            $stmt = $objNote->runQuery($query);
+            $stmt->execute();    
+        ?>
+
+        <h2>Notes</h2>
+        <div>
+            <?php if ($stmt -> rowCount() > 0) { ?>
+                <?php while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) { ?>
+                    <?php
+                        $priority_class = '';
+                        switch ($row['priority_lvl']) {
+                            case 'high':
+                                $priority_class = 'high-priority';
+                                break;
+                            case 'medium':
+                                $priority_class = 'medium-priority';
+                                break;
+                            case 'low':
+                                $priority_class = 'low-priority';
+                                break;
+                            default:
+                                $priority_class = '';
+                                break;
+                        }
+                    ?>
+                    <div class="<?php echo $priority_class; ?>">
+                        <h2><?php print $row['title']; ?></h2>
+                        <p><?php print $row['content']; ?></p>
+                        <p>Due Date: <?php print $row['due_date']; ?></p>
+                        <p>Priority Level: <?php print $row['priority_lvl']; ?></p>
+                    </div>
+                <?php } ?>
+            <?php } else { ?>
+                <p>No notes found</p>
+            <?php } ?>
+        </div>
+    </main>
+    <?php include "./includes/footer.php" ?>
 </body>
+</html>
