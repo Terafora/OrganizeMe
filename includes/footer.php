8ed961bbc19f84513ce45b576bb411b1d893c190
Charlotte Stone <charlie.stone649@gmail.com>
2024-04-09 16:51:11 +0100

    Update layout of footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,4 +1,4 @@
 <footer>
     <p>&copy; 2024, All rights reserved</p>
-    <p>Designed by <a href="https://www.linkedin.com/in/charlotte-stone-web/">Charlotte Stone</a></p>
+    <p>Designed by <a href="https://www.linkedin.com/in/charlotte-stone-web/" class="white-text">Charlotte Stone</a></p>
 </footer>
